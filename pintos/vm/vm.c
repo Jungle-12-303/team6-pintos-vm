51500ebb5b0fd32d64270c67b242d600f4e86e8a
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2026-05-11 11:43:52 +0900

    fix: 함수 네이밍 충돌 수정 및 반환값 변경
    - 함수 타입과 동일한 이름을 사용하여 발생한 에러 수정
    - 비교 함수에서 va를 비교해야 되니 주소 연산자 제거
    - page_hash_func 함수 타입과 동일하게 int64_t -> uint64_t로 수정

--- pintos/vm/vm.c
+++ pintos/vm/vm.c
@@ -4,8 +4,8 @@
 #include "vm/vm.h"
 #include "vm/inspect.h"
 
-static int64_t page_hash_func (const struct hash_elem *e, void *aux);
-static bool hash_less_func (const struct hash_elem *a, const struct hash_elem *b, void *aux);
+static uint64_t page_hash_func (const struct hash_elem *e, void *aux);
+static bool page_less_func (const struct hash_elem *a, const struct hash_elem *b, void *aux);
 
 
 /* Initializes the virtual memory subsystem by invoking each subsystem's
@@ -196,7 +196,7 @@ supplemental_page_table_kill (struct supplemental_page_table *spt UNUSED) {
 /* 새로 구현하는 함수 */
 
 /* va를 인덱스로 사용하는 해시 함수*/
-static int64_t page_hash_func (const struct hash_elem *e, void *aux) {
+static uint64_t page_hash_func (const struct hash_elem *e, void *aux) {
 	struct page *p = hash_entry(e, struct page, hash_elem);
 	uint64_t hash = hash_bytes(&p->va, sizeof p->va);
 
@@ -206,9 +206,9 @@ static int64_t page_hash_func (const struct hash_elem *e, void *aux) {
 }
 
 /* 해시 테이블에서 va를 기준으로 비교하는 함수*/
-static bool hash_less_func (const struct hash_elem *a, const struct hash_elem *b, void *aux){
+static bool page_less_func (const struct hash_elem *a, const struct hash_elem *b, void *aux){
 	struct page *p_a = hash_entry(a, struct page, hash_elem);
 	struct page *p_b = hash_entry(b, struct page, hash_elem);
 	
-	return &p_a->va < &p_b->va;
+	return p_a->va < p_b->va;
 }
