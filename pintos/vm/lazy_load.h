69bcd360e3fe61c4e69f27a1dc92a39bda528a9a
hojun-lee99 <hijun990810@gmail.com>
2026-05-18 21:59:37 +0900

    chore: ifdef 제거 및 주석 추가

--- pintos/vm/lazy_load.h
+++ pintos/vm/lazy_load.h
@@ -1,16 +1,11 @@
-#ifdef VM_LAZY_LOAD_H
-#define VM_LAZY_LOAD_H
-
 #include <stddef.h>
 #include "filesys/off_t.h"
 
 struct file;
 
 struct lazy_load_info {
-    struct file *file;
-    off_t ofs;
-    size_t page_read_bytes;
-    size_t page_zero_bytes;
+	struct file *file; 		 /* 읽어올 실행파일 */
+	off_t ofs; 				 /* page 데이터가 시작되는 파일 offset */
+	size_t page_read_bytes;  /* pgae에 파일에서 읽어 넣을 byte 수  */
+	size_t page_zero_bytes;  /* page에서 0으로 채울 byte 수 */
 };
-
-#endif
