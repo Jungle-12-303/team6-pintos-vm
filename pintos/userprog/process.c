88af1843180dd2b912977eb2889fa9f744675c5a
hojun-lee99 <hijun990810@gmail.com>
2026-05-18 22:09:56 +0900

    refactor: vm_alloc_with_initailizer 실패 시 file_close 추가

--- pintos/userprog/process.c
+++ pintos/userprog/process.c
@@ -966,6 +966,7 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
 		aux->page_zero_bytes = page_zero_bytes;
 
 		if (!vm_alloc_page_with_initializer (VM_ANON, upage, writable, lazy_load_segment, aux)) {
+			file_close(aux->file);
 			free(aux);
 			return false;
 		}
